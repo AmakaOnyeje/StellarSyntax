b12cd6c33eb56a13ce904886a96331c9faa601c4
StellarSyntax <159739586+StellarSyntax-Tech@users.noreply.github.com>
2024-08-09 15:39:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -48,15 +48,15 @@ document.body.classList.toggle('dark-mode');
 	const style =
 document.createElement('style');
 	style.textContent =
-	    .dark-mode {
+		.dark-mode {
 			background-color: #333;
 			color: #fff;
 		}
-	    .dark-mode header {
+		.dark-mode header {
 			background:
 linear-gradient(135deg, #222, #555);
 		}
-	    .dark-mode section {
+		.dark-mode section {
 			background: #444;
 			color: #fff;
 		}
